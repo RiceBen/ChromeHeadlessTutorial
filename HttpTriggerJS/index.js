2b78e4c7d3e50dc07ae80d018c821e11cb45cc67
benjamin <benjamin750715@gmail.com>
2018-10-08 01:00:47 +0800

    Remove File After Upload Finish
    remove local file after file upload to Azure Storage

--- HttpTriggerJS/index.js
+++ HttpTriggerJS/index.js
@@ -5,27 +5,34 @@ module.exports = async function (context, req) {
 
     const puppeteer = require('puppeteer');
     const screenshotStorage = require('./screenshotStorage.js');
-    
+    const fs = require('fs');
+
     await screenshotStorage.initContainer();
-    
+
     let salePage = req.body.salepage_url;
     let shoppingcarId = req.body.shoppingcar_id;
     let fileName = shoppingcarId + '.png';
 
-    (async() => {
+    (async () => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         page.setUserAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
-        
+
         await page.goto(salePage);
         await page.screenshot({
             type: 'png',
             path: fileName,
             fullPage: true
+        }).then(() => {
+            console.log('screenshot finish ready upload to storage');
+            screenshotStorage.uploadLocalFile(fileName).then(() => {
+                console.log('upload finish ready to delete file');
+                fs.unlink(fileName, (err) => {
+                    console.log(err);
+                });
+            });
         });
-        
-        screenshotStorage.uploadLocalFile(fileName);
 
         await browser.close();
-      })();
+    })();
 };

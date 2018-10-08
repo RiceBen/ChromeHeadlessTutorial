70e2dc22029a2902e56561729b8098f5f723ebe1
benjamin <benjamin750715@gmail.com>
2018-10-08 19:52:11 +0800

    Refactor to Module pattern

--- HttpTriggerJS/screenshotStorage.js
+++ HttpTriggerJS/screenshotStorage.js
@@ -1,67 +1,81 @@
-const storage = require('azure-storage');
-const path = require('path');
+module.exports = (function () {
+    let storage = require('azure-storage');
+    let path = require('path');
 
-const blobService = storage.createBlobService();
-const containerName = "chromeheadlessdemo";
+    let blobService = storage.createBlobService();
+    let containerName = "chromeheadlessdemo";
 
-/**
- * create Azure Storage container
-*/
-async function createContainer() {
-    return new Promise((resolve, reject) => {
-        blobService.createContainerIfNotExists(containerName, { publicAccessLevel: 'blob' }, err => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ message: `Container '${containerName}' created` });
-            }
+    /**
+     * * create Azure Storage container
+     * */
+    let createContainer = async function () {
+        return new Promise((resolve, reject) => {
+            blobService.createContainerIfNotExists(containerName, { publicAccessLevel: 'blob' }, err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve({ message: `Container '${containerName}' created` });
+                }
+            });
         });
-    });
-};
-
-/**
- * list all Azure Storage container
-*/
-async function listContainers() {
-    return new Promise((resolve, reject) => {
-        blobService.listContainersSegmented(null, (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ message: `${data.entries.length} containers`, containers: data.entries });
-            }
-        });
-    });
-};
-
-/**
- * upload file to Azure Storage
- * 
- * @param {string} filePath file path which will be upload to Azure Storage
-*/
-exports.uploadLocalFile = async function (filePath) {
-    return new Promise((resolve, reject) => {
-        const fullPath = path.resolve(filePath);
-        const blobName = path.basename(filePath);
-        blobService.createBlockBlobFromLocalFile(containerName, blobName, fullPath, err => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ message: `Local file "${filePath}" is uploaded` });
-            }
-        });
-    });
-};
-
-/**
- * initionalize Azure Storage container
-*/
-exports.initContainer = async function () {
-    let response = await listContainers();
-    response.containers.forEach((container) => console.log(` -  ${container.name}`));
-    let containerDoesNotExist = response.containers.findIndex((container) => container.name === containerName) === -1;
-    if (containerDoesNotExist) {
-        await createContainer(containerName);
-        console.log(`Container "${containerName}" is created`);
     }
-};
+
+    /**
+     * * list all Azure Storage container
+     * */
+    let listContainers = async function () {
+        return new Promise((resolve, reject) => {
+            blobService.listContainersSegmented(null, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve({ message: `${data.entries.length} containers`, containers: data.entries });
+                }
+            });
+        });
+    }
+
+    /**
+     * * upload file to Azure Storage
+     * * 
+     * * @param {string} filePath file path which will be upload to Azure Storage
+     * */
+    this.uploadLocalFile = async function (filePath) {
+        return new Promise((resolve, reject) => {
+            const fullPath = path.resolve(filePath);
+            const blobName = path.basename(filePath);
+            blobService.createBlockBlobFromLocalFile(containerName, blobName, fullPath, err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve({ message: `Local file "${filePath}" is uploaded` });
+                }
+            });
+        });
+    }
+
+    /**
+     * * initionalize Azure Storage container
+     * */
+    this.initContainer = async function () {
+        let response = await listContainers();
+        response.containers.forEach((container) => console.log(` -  ${container.name}`));
+        let containerDoesNotExist = response.containers.findIndex((container) => container.name === containerName) === -1;
+        if (containerDoesNotExist) {
+            await createContainer(containerName);
+            console.log(`Container "${containerName}" is created`);
+        }
+    }
+    return {
+        /**
+         * * initionalize Azure Storage container
+         * */
+        initContainer: initContainer,
+        /**
+         * * upload file to Azure Storage
+         * * 
+         * * @param {string} filePath file path which will be upload to Azure Storage
+         * */
+        uploadLocalFile: uploadLocalFile
+    }
+}());
